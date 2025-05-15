2e97b7ec56f63efe7eed578ed2b998c1c90608c5
jeongungeom <jeongung.eom@northstar.co.kr>
2025-05-15 15:17:16 +0900

    feat: ë¦r

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -9,15 +9,30 @@ import Profile from '../components/Profile.vue'
 
 const routes = [
     { path: '/', component: IgLogin },
-    { path: '/feed', component: Feed},
-    { path: '/signUp', component: SignUp},
-    { path: '/update', component: Update},
-    { path: '/profile', component: Profile},
+    { path: '/feed', component: Feed, meta: {requiresAuth: true} },
+    { path: '/signUp', component: SignUp, meta: {requiresAuth: true} },
+    { path: '/update', component: Update, meta: {requiresAuth: true} },
+    { path: '/profile', component: Profile, meta: {requiresAuth: true} },
 ]
 
 const router = createRouter({
     history: createWebHistory(),
     routes,
 })
+
+router.beforeEach((to, from, next) => {
+    const isLoggedIn = !!localStorage.getItem('jwt')
+    if (to.matched.some(record => record.meta && record.meta.requiresAuth)) {
+        if (!isLoggedIn) {
+            alert('로그인 부탁드립니다.')
+            next('/')
+        } else {
+            next()
+        }
+    } else {
+        next()
+    }
+})
+
 
 export default router
