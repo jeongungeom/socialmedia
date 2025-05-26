9fb75f4459c42c7caf016fc938343bd9175cca24
jeongungeom <jeongung.eom@northstar.co.kr>
2025-05-26 13:18:56 +0900

    style: 스타일 변경

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,23 +1,28 @@
 <template>
-  <div id="app" :class="{'has-sidebar': !isLoginPage}">
-    <Sidebar v-if="!isLoginPage" />
+  <div id="app" :class="{'has-sidebar': !isHideSidebarPage}">
+    <Sidebar v-if="!isHideSidebarPage" />
     <router-view />
   </div>
 </template>
 
 <script setup>
-import {computed, ref} from 'vue'
+import { computed } from 'vue'
 import { useRoute } from 'vue-router'
-import Sidebar from "./components/Sidebar.vue";
+import Sidebar from "./components/Sidebar.vue"
 
 const route = useRoute()
-const isLoginPage = computed(() => route.path === '/')
 
+const hideSidebarPaths = ['/', '/signup', '/signUp']
+
+const isHideSidebarPage = computed(() => hideSidebarPaths.includes(route.path))
 </script>
 
-
-<style>
-body {
+<style scoped>
+html, body, #app {
   background: #000 !important;
+  min-height: 100vh;
+  margin: 0;
+  padding: 0;
 }
 </style>
+
